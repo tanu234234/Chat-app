fb3be72f41d761d2089a39df7f66f9c2723496af
tanu234234 <tanushree9663@gmail.com>
2026-03-22 18:59:16 +0530

    Added user name feature

--- public/script.js
+++ public/script.js
@@ -1,15 +1,22 @@
 const socket = io();
 
 function sendMessage() {
-    const input = document.getElementById("messageInput");
-    const message = input.value;
 
-    socket.emit("chat message", message);
-    input.value = "";
+    const username = document.getElementById("username").value;
+    const message = document.getElementById("messageInput").value;
+
+    const fullMessage = username + ": " + message;
+
+    socket.emit("chat message", fullMessage);
+
+    document.getElementById("messageInput").value = "";
 }
 
 socket.on("chat message", function(msg) {
+
     const li = document.createElement("li");
     li.textContent = msg;
+
     document.getElementById("messages").appendChild(li);
+
 });
